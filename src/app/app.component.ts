d00ca9b1fc0b78fbc4f11596954d27550a9d5508
Lukk17 <luksarna@gmail.com>
2019-02-07 23:00:48 +0100

    new feature like string interpolatation and parameter binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // this selector is used in index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server created';
+
+  constructor() {
+    // after given time (2 secs) parameter will change to true changing html button
+    setTimeout( () => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+}
