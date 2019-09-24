d2fe6c20a0a9a48a4ebcb083fa92844ce1581e23
Lukk <luksarna@gmail.com>
2019-09-24 17:46:05 +0200

    logger service

--- src/app/offers/offers-list/offers-list.component.ts
+++ src/app/offers/offers-list/offers-list.component.ts
@@ -1,12 +1,15 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Offer} from "../offer.model";
-import {AppComponent} from "../../app.component";
-import {of} from "rxjs";
+import {LoggingService} from "../../services/logging.service";
 
 @Component({
   selector: 'app-offers-list',
   templateUrl: './offers-list.component.html',
-  styleUrls: ['./offers-list.component.css']
+  styleUrls: ['./offers-list.component.css'],
+  //  if added here new instance of service will be crated
+  //  if want one instance for all component add it only to main appcomponent and not its child as providers
+  //  children should only have it in constructor
+  providers: [LoggingService]
 })
 export class OffersListComponent implements OnInit {
   @Output() readyToBook = new EventEmitter<Offer>();
@@ -17,7 +20,7 @@ export class OffersListComponent implements OnInit {
     new Offer('hilton', 'hotel', 'https://www3.hilton.com/resources/media/hi/HROBCHH/en_US/img/shared/full_page_image_gallery/main/HH_extsign_1270x560_FitToBoxSmallDimension_Center.jpg')
   ];
 
-  constructor() {
+  constructor(private logger : LoggingService) {
   }
 
   ngOnInit() {
@@ -25,5 +28,6 @@ export class OffersListComponent implements OnInit {
 
   bookOffer(offer: Offer) {
     this.readyToBook.emit(offer);
+    this.logger.log(offer.name + " booked");
   }
 }
---
+++ src/app/services/logging.service.ts
@@ -0,0 +1,7 @@
+export class LoggingService {
+
+  log(message: string) {
+    console.log(message);
+  }
+
+}
