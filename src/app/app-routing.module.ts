0c4a2807e7bf51a5992c462bf52c8a0e54f63776
Lukk <luksarna@gmail.com>
2019-11-06 17:33:25 +0100

    router guard added to check authentication and redirect if not logged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {NgModule} from "@angular/core";
 import {FormComponent} from "./form/form.component";
 import {FirebaseComponent} from "./firebase/firebase.component";
 import {AuthComponent} from "./auth/auth.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 //  in app.component.html there must be added
 //  <router-outlet></router-outlet>
@@ -13,17 +14,23 @@ import {AuthComponent} from "./auth/auth.component";
 //
 //  also add routerLink="/path"
 //  as links in html file for example in header
-const appRoutes: Routes =[
+const appRoutes: Routes = [
   {path: '', redirectTo: "/offers", pathMatch: "full"},
-  {path: 'offers', component: OffersComponent, children: [
+  {
+    path: 'offers', component: OffersComponent, children: [
       // for offers detail link, ":name" where name can be anything
       {path: ':name/:desc', component: OfferDetailComponent},
-    ]},
+    ]
+  },
   {path: 'extras', component: ExtrasComponent},
   {path: 'form', component: FormComponent},
-  {path: 'firebase', component: FirebaseComponent},
+  {
+    path: 'firebase',
+    component: FirebaseComponent,
+    canActivate: [AuthGuard],
+  },
   {path: 'auth', component: AuthComponent}
-]
+];
 
 @NgModule({
   imports: [
@@ -32,7 +39,6 @@ const appRoutes: Routes =[
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
 import {LoadingComponent} from './loading/loading.component';
 import {InterceptorService} from "./firebase/interceptor.service";
+import {AuthGuard} from "./auth/auth.guard";
 
 
 @NgModule({
@@ -61,7 +62,8 @@ import {InterceptorService} from "./firebase/interceptor.service";
       provide: HTTP_INTERCEPTORS,
       useClass: InterceptorService,
       multi: true
-    }
+    },
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
